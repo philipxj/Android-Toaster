79b3d47e72cce519066e65c62967c94412841da7
philipxj <philipxjxjxj@gmail.com>
2020-06-22 12:24:04 +0800

    Initial Commit

---
+++ toastermessage/src/main/java/com/philipxj/toastermessage/ToasterMessage.java
@@ -0,0 +1,14 @@
+package com.philipxj.toastermessage;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+
+    public static void ToasterShort(Context context,String string){
+        Toast.makeText(context,string,Toast.LENGTH_SHORT).show();
+    }
+    public static void ToasterLong(Context context,String string){
+        Toast.makeText(context,string,Toast.LENGTH_LONG).show();
+    }
+}
